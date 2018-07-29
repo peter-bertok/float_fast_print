6032201955e03fa297f68b6981fc9cab68249079
Peter Bertok <peter@bertok.com>
2018-07-29 10:44:10 +1000

    Preparing for the f64 code

--- README.md
+++ README.md
@@ -2,26 +2,25 @@
 A Rust version of the Ryū float-to-string conversion algorithm by Ulf Adams of Google Germany.  
 
 ## Description
+The purpose of this crate is to improve the speed of converting `f32` and `f64` to string representations, 
+which is particularly important when performing high-performance serialisation of JSON, CSV, XLSX, and 
+scientific data formats where all numbers are internally treated as floating point. In some cases, this
+can be the bottleneck to serialisation.
 
-The goal of this project is to improve the speed of converting `f32` and `f64` to and from string
-representations, which is particularly important when performing high-performance serialisation of
-JSON where all numbers are internally treated as `f64`.
-
-Additionally, during development of this code it was noted that the [serde_json](https://github.com/serde-rs/json/) crate
+During development of this code it was noted that the [serde_json](https://github.com/serde-rs/json/) crate
 references the [dtoa](https://github.com/dtoa-rs/) crate, which is both slower and contains unsafe
 code. This crate uses no unsafe code, and could potentially use `#![no_std]` as well.
 
 ## Status
-
-This library is currently very much in the work-in-progress state, and is not yet usable. However,
-things are looking promising, with the Ryū `bench_float_to_decimal_common_shortest` function absolutely
+This crate is currently very much in the work-in-progress state, and is not yet usable. However,
+things are looking promising, with the Ryū `bench_write_f32_shortest` function absolutely
 smoking the currently available alternatives:
 
 ```
-test tests::bench_dtoa                             ... bench:          45 ns/iter (+/- 1)
-test tests::bench_f32_debug                        ... bench:         108 ns/iter (+/- 0)
-test tests::bench_f32_format                       ... bench:         109 ns/iter (+/- 0)
-test tests::bench_float_to_decimal_common_shortest ... bench:          25 ns/iter (+/- 0)
+test tests::bench_dtoa               ... bench:          45 ns/iter (+/- 1)
+test tests::bench_f32_debug          ... bench:         108 ns/iter (+/- 0)
+test tests::bench_f32_format         ... bench:         109 ns/iter (+/- 0)
+test tests::bench_write_f32_shortest ... bench:          25 ns/iter (+/- 0)
 ```
 
 ## References
--- tests/benchmarks.rs
+++ tests/benchmarks.rs
@@ -6,13 +6,12 @@ extern crate dtoa;
 
 use ryu::*;
 use test::Bencher;
-use dtoa::*;
 use std::io::Write;
 
 #[bench]
 fn bench_f32_debug(b: &mut Bencher) {
     let mut buffer : Vec<u8> = Vec::with_capacity( 32 );
-    let x: f32 = 215.1291248e-43;
+    let x: f32 = -28215.12291248e-43;
 
     b.iter(|| {
         buffer.clear();
@@ -23,7 +22,7 @@ fn bench_f32_debug(b: &mut Bencher) {
 #[bench]
 fn bench_f32_format(b: &mut Bencher) {
     let mut buffer : Vec<u8> = Vec::with_capacity( 32 );
-    let x: f32 = 215.1291248e-43;
+    let x: f32 = -28215.12291248e-43;
 
     b.iter(|| {
         buffer.clear();
@@ -31,11 +30,32 @@ fn bench_f32_format(b: &mut Bencher) {
     });
 }
 
+#[bench]
+fn bench_f64_debug(b: &mut Bencher) {
+    let mut buffer : Vec<u8> = Vec::with_capacity( 32 );
+    let x: f64 = -28215.12291248e-43;
+
+    b.iter(|| {
+        buffer.clear();
+        write!(&mut buffer, "{:?}", x);
+    });
+}
 
 #[bench]
-fn bench_float_to_decimal_common_shortest(b: &mut Bencher) {
+fn bench_f64_format(b: &mut Bencher) {
     let mut buffer : Vec<u8> = Vec::with_capacity( 32 );
-    let x: f32 = 215.1291248e-43;
+    let x: f64 = -28215.12291248e-43;
+
+    b.iter(|| {
+        buffer.clear();
+        write!(&mut buffer, "{}", x);
+    });
+}
+
+#[bench]
+fn bench_write_f32_shortest(b: &mut Bencher) {
+    let mut buffer : Vec<u8> = Vec::with_capacity( 32 );
+    let x: f32 = -28215.12291248e-43;
 
     b.iter(|| {
         buffer.clear();
@@ -45,13 +65,36 @@ fn bench_float_to_decimal_common_shortest(b: &mut Bencher) {
 }
 
 #[bench]
-fn bench_dtoa(b: &mut Bencher) {
+fn bench_write_f64_shortest(b: &mut Bencher) {
     let mut buffer : Vec<u8> = Vec::with_capacity( 32 );
-    let x: f32 = 215.1291248e-43;
+    let x: f64 = -28215.12291248e-43;
 
     b.iter(|| {
         buffer.clear();
-        let _ = write( &mut buffer, x).unwrap();
+
+        let _ = write_f64_shortest(&mut buffer, x).unwrap();
+    });
+}
+
+#[bench]
+fn bench_dtoa_f64(b: &mut Bencher) {
+    let mut buffer : Vec<u8> = Vec::with_capacity( 32 );
+    let x: f64 = -28215.12291248e-43;
+
+    b.iter(|| {
+        buffer.clear();
+        let _ = dtoa::write( &mut buffer, x).unwrap();
+    });
+}
+
+#[bench]
+fn bench_dtoa_f32(b: &mut Bencher) {
+    let mut buffer : Vec<u8> = Vec::with_capacity( 32 );
+    let x: f32 = -28215.12291248e-43;
+
+    b.iter(|| {
+        buffer.clear();
+        let _ = dtoa::write( &mut buffer, x).unwrap();
     });
 }
 
